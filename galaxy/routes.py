09f084bcc7bac84c0ec1eddfc22b02ff39b15107
Dan-Matthews-23 <131537857+Dan-Matthews-23@users.noreply.github.com>
2026-04-06 10:39:49 +0000

    MOVED static files and folders to correct dir, UPDATED routes

--- galaxy/routes.py
+++ galaxy/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
-from galaxy import app, db
+from galaxy import app
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to the Galaxy</h1><p>The routing is working!</p>"
+    return render_template("index.html")
