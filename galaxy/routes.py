9b1731943fd82fcb84886f083fc5664718fd0218
Dan-Matthews-23 <131537857+Dan-Matthews-23@users.noreply.github.com>
2026-04-08 08:49:11 +0000

    UPDATED routes.py with new password hash. MIGRATED initial user table

--- galaxy/routes.py
+++ galaxy/routes.py
@@ -1,7 +1,9 @@
 
-from galaxy import app
+from galaxy import app, db, bcrypt  # <--- Make sure 'bcrypt' is added here!
 from flask import render_template, url_for, flash, redirect
 from galaxy.forms import RegistrationForm
+from galaxy.models import User  # <--- Check if this line is missing!
+
 
 @app.route("/")
 def home():
@@ -10,9 +12,17 @@ def home():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    # This checks if the user clicked 'Submit' AND if the data is valid
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        # Scramble the password!
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        
+        user = User(username=form.username.data, 
+                    email=form.email.data, 
+                    password=hashed_password) # Save the SCRAMBLED version
+        
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('home'))
-    
+        
     return render_template('register.html', title='Register', form=form)
