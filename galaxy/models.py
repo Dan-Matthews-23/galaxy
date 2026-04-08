11b5a42edce6fd011d783f009eed9208cccaffbc
Dan-Matthews-23 <131537857+Dan-Matthews-23@users.noreply.github.com>
2026-04-08 08:40:44 +0000

    UPDATED models.py with user model

---
+++ galaxy/models.py
@@ -0,0 +1,16 @@
+from galaxy import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    # 60 characters is standard for a hashed password (Bcrypt)
+    password = db.Column(db.String(60), nullable=False)
+
+    ### RELATIONSHIP POINTERS (Commented out until tables are built) ###
+    # income = db.relationship('Income', backref='owner', uselist=False)
+    # military = db.relationship('Military', backref='owner', uselist=False)
+    # exploration = db.relationship('Exploration', backref='owner', uselist=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
