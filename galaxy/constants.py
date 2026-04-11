c1b7e22994bc50f98efdbc9a78ed2b73b08590ce
Dan-Matthews-23 <131537857+Dan-Matthews-23@users.noreply.github.com>
2026-04-11 09:30:36 +0000

    CREATED constants.py for game variables

---
+++ galaxy/constants.py
@@ -0,0 +1,18 @@
+# constants.py
+
+CIV_DATA = {
+    1: {
+        'name': 'Human',
+        'income_mod': 1.0,
+        'science_mod': 1.0,
+        'combat_mod': 1.0,
+        'description': 'Versatile and ambitious.'
+    },
+    2: {
+        'name': 'Android',
+        'income_mod': 0.8,
+        'science_mod': 1.2,
+        'combat_mod': 1.1,
+        'description': 'Efficient but high maintenance.'
+    }
+}
